4c91fcb50d7bdeb34cbb8de2b475d5409adf3059
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-05-15 15:49:22 -0300

    criando cactus

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const DINO = document.querySelector(".dino");
+const BACKGROUND = document.querySelector('.background');
+
+let isJumping = false;
 
 /**
  * Método para identificar a tecla precionada espaço
  */
 function handleKeyUp(event) {
     if(event.keyCode === 32){
-        console.log('pressinou espaço');
-        jump();
+        if(!isJumping){
+            jump();
+        }
     }
 }
 
@@ -15,6 +19,7 @@ function handleKeyUp(event) {
  */
 function jump() {
     let position = 0;
+    isJumping = true;
 
     /* setInterval: define intervalos, tudo que estiver dentro vai ser executado
         a cada 20 milisegundos
@@ -27,6 +32,7 @@ function jump() {
             let downInterval = setInterval(() =>{
                 if(position <= 0){
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                     position -= 20;
                     DINO.style.bottom = position + 'px';
@@ -41,7 +47,15 @@ function jump() {
     }, 20);
 }
 
-/**
- * 
- */
+/**criando cactus */
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    BACKGROUND.appendChild(cactus);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
